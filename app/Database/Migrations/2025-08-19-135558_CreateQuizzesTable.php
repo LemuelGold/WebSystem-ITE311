01e96df68aa9eed4fba53a6c1e14ba3ed7684ba2
Carlo-Angelo-T-Fundar <carloangelofundar@gmail.com>
2025-08-20 00:52:18 +0800

    Added database migrations and seeders

---
+++ app/Database/Migrations/2025-08-19-135558_CreateQuizzesTable.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateQuizzesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'lesson_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'question' => [
+                'type' => 'TEXT',
+            ],
+            'option_a' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'option_b' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'option_c' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'option_d' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'correct_answer' => [
+                'type'       => 'ENUM',
+                'constraint' => ['A', 'B', 'C', 'D'],
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('lesson_id', 'lessons', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('quizzes');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('quizzes');
+    }
+}
