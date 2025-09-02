b8f924f6a4c548ab9aad841e1d97c1bbaa496da0
Carlo-Angelo-T-Fundar <carloangelofundar@gmail.com>
2025-09-02 21:11:56 +0800

    Initial commit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'lms_fundar',
+        'database'     => 'G3_WebSystem',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
